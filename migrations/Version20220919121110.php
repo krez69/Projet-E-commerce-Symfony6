424691db29059f827d320d7df85edab7ef2a7f3d
krez69 <hamimed.walid@gmail.com>
2023-03-16 18:11:33 +0100

    recuperation mot de passe

--- migrations/Version20220919121110.php
+++ migrations/Version20220919121110.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220802100907 extends AbstractMigration
+final class Version20220919121110 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20220802100907 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE users ADD is_verified TINYINT(1) NOT NULL');
+        $this->addSql('ALTER TABLE users ADD reset_token VARCHAR(100) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE users DROP is_verified');
+        $this->addSql('ALTER TABLE users DROP reset_token');
     }
 }
---
+++ src/Form/ResetPasswordRequestFormType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+
+class ResetPasswordRequestFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'label' => 'Entrer votre e-mail',
+                'attr' => [
+                    'placeholder' => 'exemple@email.com',
+                    'class' => 'form-control',
+                    ]
+                
+                ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
